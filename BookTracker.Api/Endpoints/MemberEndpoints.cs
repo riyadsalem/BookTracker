f20f09b299879295da4c7951859d0860de1a1125
riyad salem <riyad.m.salem.19988@gmail.com>
2026-07-06 15:29:33 +0200

    Members toevoegen [...arr, MemberEndpoints] is klaar

---
+++ BookTracker.Api/Endpoints/MemberEndpoints.cs
@@ -0,0 +1,60 @@
+using BookTracker.Api.Application.Members.CreateMember;
+using BookTracker.Api.Application.Members.DeleteMember;
+using BookTracker.Api.Application.Members.GetMemberDetails;
+using BookTracker.Api.Application.Members.GetMemberSummaries;
+using BookTracker.Api.Application.Members.UpdateMember;
+using BookTracker.Api.Domain;
+
+namespace BookTracker.Api.Endpoints.Members;
+
+public static class MemberEndpoints
+{
+    public static IEndpointRouteBuilder MapMemberEndpoints(this IEndpointRouteBuilder app)
+    {
+        app.MapGet("/members", GetMemberSummaries);
+        app.MapGet("/members/{id:int}", GetMemberDetails);
+        app.MapPost("/members", CreateMember);
+        app.MapPut("/members/{id:int}", UpdateMember);
+        app.MapDelete("/members/{id:int}", DeleteMember);
+
+        return app;
+    }
+
+    public static async Task<IResult> GetMemberSummaries([AsParameters] GetMemberSummariesRequest request, GetMemberSummariesQueryHandler handler) =>
+     Results.Ok(await handler.Execute(request));
+
+
+    public static async Task<IResult> GetMemberDetails(int id, GetMemberDetailsQueryHandler query)
+    {
+        GetMemberDetailsResponse? member = await query.Execute(id);
+        return member is null ? Results.NotFound() : Results.Ok(member);
+    }
+
+    public static async Task<IResult> CreateMember(CreateMemberRequest request, CreateMemberCommandHandler handler)
+    {
+        try
+        {
+            CreateMemberResponse response = await handler.Execute(request);
+            return Results.Created($"/members/{response.Id}", response);
+        }
+        catch (DomainException exception)
+        {
+            return Results.BadRequest(new { error = exception.Message });
+        }
+    }
+    public static async Task<IResult> UpdateMember(int id, UpdateMemberRequest request, UpdateMemberCommandHandler handler)
+    {
+        try
+        {
+            return await handler.Execute(id, request) ? Results.NoContent() : Results.NotFound();
+        }
+        catch (DomainException exception)
+        {
+            return Results.BadRequest(new { error = exception.Message });
+        }
+    }
+
+    public static async Task<IResult> DeleteMember(int id, DeleteMemberCommandHandler handler) =>
+    await handler.Execute(id) ? Results.NoContent() : Results.NotFound();
+
+}

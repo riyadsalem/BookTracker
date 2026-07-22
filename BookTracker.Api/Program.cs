d455b53558df3afe1d4e786175a166b8aeab8f70
riyad salem <riyad.m.salem.19988@gmail.com>
2026-07-22 14:10:48 +0200

    run book tracker with docker compose

--- BookTracker.Api/Program.cs
+++ BookTracker.Api/Program.cs
@@ -4,12 +4,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.AddBookTracker();
 
+var frontendOrigin = builder.Configuration["FrontendOrigin"]
+    ?? "http://localhost:5173";
+/*
+http://localhost:5173 >>> Local Development ( nu weerkt react by Vite DUS (5173))
+http://localhost:3000 >>> Docker  (nu weerkt react by Nginx DUS (3000))
+https://booktracker.com >>> Production
+*/
+
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
         policy
-            .WithOrigins("http://localhost:5173")
+            .WithOrigins(frontendOrigin)
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
